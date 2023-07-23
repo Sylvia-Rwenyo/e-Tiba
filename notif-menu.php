6c01cbb6a39fd0e22f9506fee85e273124b6a7a0
BeMa99 <berclaym@gmail.com>
2023-07-23 15:31:59 +0300

    notification on dashboard

--- notif-menu.php
+++ notif-menu.php
@@ -1,8 +1,34 @@
     <div class="menu meet-menu">
         <ul>
-            <li><a href=""><i class="fa fa-bell"></i></a></li>
-            <li><a href="meet.php"><i class="fa-solid fa-video"></i></a></li>
+            <li><a href="meet.php"><i class="fa fa-bell"></i></a></li>
             <li><a href=""><i class="fa fa-message"></i></a></li>
+            <li>
+                <?php 
+                session_start();
+                ?>
+                <a href = <?php 
+                if($_SESSION['category'] == 'doctor')
+                {
+                    echo "doctors/patient-doctor-chat.php";
+                }
+                elseif($_SESSION['category'] == 'patient')
+                {
+                    echo "individual-patient-chat.php";
+                }?>>
+                <?php
+                $resultPost = mysqli_query($conn,"SELECT readStatus FROM chat WHERE sent_to = '$current_user_email'");
+                $count = 0;
+                while($row = mysqli_fetch_array($resultPost)) 
+                {
+                    if($row['readStatus'] == 'unread')
+                    {
+                        $count = $count + 1;
+                    }?>
+                    <i class="fa-solid fa-video"><?php if($count == 0){echo "";}else{echo $count;}?></i><?php 
+                }?>
+                </a>
+                </li>
+            
             <li><a href=""><i class="fa-solid fa-book"></i></a></li>
         </ul>
     </div>
