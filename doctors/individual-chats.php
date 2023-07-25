5c494fd4a6b5c0f236f2835f7f623dadbffcda1a
BeMa99 <berclaym@gmail.com>
2023-07-25 22:59:08 +0300

    speech bubbles

--- doctors/individual-chats.php
+++ doctors/individual-chats.php
@@ -48,7 +48,7 @@
                 {
                     if ($row['readStatus'] == 'read')
                     { ?>
-                        <div class="message-box" style="<?php if($current_user_email == $row["emailAddress"]){echo 'text-align:right;margin-left:40%;margin-right:20%;';}else{echo  'text-align:left;margin-right:40%;margin-left:20%;';}?>">
+                        <div class="message-box tri-right-left-top" style="<?php if($current_user_email == $row["emailAddress"]){echo 'text-align:right;margin-left:40%;margin-right:20%;';}else{echo  'text-align:left;margin-right:40%;margin-left:20%;';}?>">
                             <p><?php echo $row["message"]; ?></p>
                             <form id="message-form"  action="../controls/processing.php?id=<?php echo $row["id"]; ?>" method="POST" style="<?php if($current_user_email == $row["emailAddress"]){echo 'display:block;';}else{echo  'display:none;';}?>">
                                 <input type="hidden"  name="sent_to" value="<?php echo $sent_to;?>"/>    
@@ -75,12 +75,23 @@
                 else
                 {
                     ?>
-                        <div class="message-box" style="<?php if($current_user_email == $row["emailAddress"]){echo 'text-align:right;margin-left:40%;margin-right:20%;';}else{echo  'text-align:left;margin-right:40%;margin-left:20%;';}?>">
+                        <div class="message-box tri-right-right-top" style="<?php if($current_user_email == $row["emailAddress"]){echo 'text-align:right;margin-left:40%;margin-right:20%;';}else{echo  'text-align:left;margin-right:40%;margin-left:20%;';}?>">
                             <p><?php echo $row["message"]; ?></p>
-                            <form id="message-form"  action="../controls/processing.php?id=<?php echo $row["id"]; ?>" method="POST" style="<?php if($current_user_email == $row["emailAddress"]){echo 'display:block;';}else{echo  'display:none;';}?>">
-                                <input type="hidden"  name="sent_to" value="<?php echo $sent_to;?>"/>    
-                                <input id = "message-delete-doc" type="submit" name="message-delete-doc" class="chat-btn" value="Delete"/>
-                            </form>
+                            <div style="display:flex;flex-direction:row;">
+                                <form id="message-form"  action="../controls/processing.php?id=<?php echo $row["id"]; ?>" method="POST" style="<?php if($current_user_email == $row["emailAddress"]){echo 'display:block;';}else{echo  'display:none;';}?>">
+                                    <input type="hidden"  name="sent_to" value="<?php echo $sent_to;?>"/>    
+                                    <input id = "message-delete-doc" type="submit" name="message-delete-doc" class="chat-btn" value="Delete"/>
+                                </form>
+                                <img class = "read_ticks_in_chat" size = "5px" src="<?php 
+                                if($row["readStatus"] == 'unread')
+                                {
+                                    echo "../images/grey_tick.PNG";
+                                }
+                                else
+                                {
+                                    echo "../images/blue_tick.PNG";
+                                }?>"/>
+                            </div>
                         </div> 
                         <?php
                 }
