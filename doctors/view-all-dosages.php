8fcaa1506477c843fa9dcbfbea465847a9e0d952
BeMa99 <berclaym@gmail.com>
2023-08-02 12:09:39 +0300

    updated dosage view all info

--- doctors/view-all-dosages.php
+++ doctors/view-all-dosages.php
@@ -37,7 +37,9 @@
         <div class="dosage_records">
             <table>
                 <tr class="table_field_names">
+                    <th>Patient name</th>
                     <th>Medicine name</th>
+                    <th>Administered By</th>
                     <th>Number of Tablets</th>
                     <th>Number of Days</th>
                     <th>Number of Times a Day</th>
@@ -50,8 +52,14 @@
                 ?>
                 <tr class="table_field_items">
                     <td>
+                    <?php echo $row["patientName"]; ?>
+                    </td> 
+                    <td>
                     <?php echo $row["dosageName"]; ?>
                     </td> 
+                    <td>
+                    <?php echo $row["attending_doctor_name"]; ?>
+                    </td>
                     <td>
                     <?php echo $row["tablets"]; ?>
                     </td>
