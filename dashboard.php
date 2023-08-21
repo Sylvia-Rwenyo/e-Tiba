e1f433c6464ab7c7c6b818af10e1f43a8a9e0e86
BeMa99 <berclaym@gmail.com>
2023-08-21 07:31:04 +0300

    added chart specific to patients

--- dashboard.php
+++ dashboard.php
@@ -35,15 +35,26 @@ if(isset($_SESSION["loggedIN"])){
     </div>
     ';
 }
+if($_SESSION["category"] != "patient"){
   include_once 'patient-progress-search-div.php';
-  if(isset($_GET['id'])){
-    $requested_patient = $_GET['id'];?>
-    <div class="progress-charts">
-      <div class="sleep_chart" id="sleep_chart"></div>
-      <div class="meals_chart" id="meals_chart"></div>
+}
+
+if(isset($_GET['id'])){
+  $requested_patient = $_GET['id'];?>
+  <div class="progress-charts">
+    <div class="sleep_chart" id="sleep_chart"></div>
+    <div class="meals_chart" id="meals_chart"></div>
   </div>
-  <?php
-  }
+<?php
+}
+elseif($_SESSION["category"] == "patient"){
+  $requested_patient = $_SESSION['id'];?>
+  <div class="progress-charts">
+    <div class="sleep_chart" id="sleep_chart"></div>
+    <div class="meals_chart" id="meals_chart"></div>
+  </div>
+<?php
+}
 ?>
     
 </div>
