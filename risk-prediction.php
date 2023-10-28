11a5db4e10a603db09b56b731061da8794fe33a6
SylviaRwenyo <rwenyosylvia@gmail.com>
2023-10-28 22:37:53 +0300

    risk prediction updates

--- risk-prediction.php
+++ risk-prediction.php
@@ -1,4 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Phpml\Classification\KNearestNeighbors;
+
 include_once 'conn.php';
 
 // Initialize empty arrays to store data for each day
@@ -15,85 +19,95 @@ $totalExercise = 0;
 $totalMeals = 0;
 $totalSleep = 0;
 
-    // Initialize data variables for the current day
-    $sleepTime = 0;
-    $mealNumbers = 0;
-    $exerciseTime = 0;
+// Initialize data variables for the current day
+$sleepTime = 0;
+$mealNumbers = 0;
+$exerciseTime = 0;
 
-    // Fetch and calculate data for each day here
+// Fetch and calculate data for each day here
 
-    // Sleep data
-    $sql2 = "SELECT * FROM patientsleeplog WHERE userID = '$userID'";
-    $result2 = mysqli_query($conn, $sql2);
-    if (mysqli_num_rows($result2) > 0) {
-        while ($row = mysqli_fetch_array($result2)) {
-            $sleepTime = $row['sleepTime'];
-            $totalSleep += $sleepTime;
-        }
-
-        $targetSleep = 12;
-        $sleepPercentage = 100 * $sleepTime / $targetSleep;
-
-        // Store sleep data for this day
-        $sleepData[] = $sleepTime;
+// Sleep data
+$sql2 = "SELECT * FROM patientsleeplog WHERE userID = '$userID'";
+$result2 = mysqli_query($conn, $sql2);
+if (mysqli_num_rows($result2) > 0) {
+    while ($row = mysqli_fetch_array($result2)) {
+        $sleepTime = $row['sleepTime'];
+        $totalSleep += $sleepTime;
     }
 
-    // Meals data
-    $stmt = "SELECT * FROM patientsmeallog WHERE userID='$userID'  ";
-    $sql3 = mysqli_query($conn, $stmt);
+    $targetSleep = 12;
+    $sleepPercentage = 100 * $sleepTime / $targetSleep;
 
-    if (mysqli_num_rows($sql3) > 0) {
-        while ($row = mysqli_fetch_array($sql3)) {
-            $mealNumbers += 1;
-            $totalMeals += $mealNumbers;
-        }
+    // Store sleep data for this day
+    $sleepData[] = $sleepTime;
+}
 
-        $targetMeals = 3;
-        $mealsPercentage = 100 * $mealNumbers / $targetMeals;
+// Meals data
+$stmt = "SELECT * FROM patientsmeallog WHERE userID='$userID'  ";
+$sql3 = mysqli_query($conn, $stmt);
 
-        // Store meals data for this day
-        $mealsData[] = $mealNumbers;
+if (mysqli_num_rows($sql3) > 0) {
+    while ($row = mysqli_fetch_array($sql3)) {
+        $mealNumbers += 1;
+        $totalMeals += $mealNumbers;
     }
 
-    // Exercise data
-    $stmt = "SELECT * FROM patientsexerciselog WHERE userID='$userID'  ";
-    $sql3 = mysqli_query($conn, $stmt);
+    $targetMeals = 3;
+    $mealsPercentage = 100 * $mealNumbers / $targetMeals;
 
-    if (mysqli_num_rows($sql3) > 0) {
-        while ($row = mysqli_fetch_array($sql3)) {
-            $exerciseTime = $row['exerciseDuration'];
-            $totalExercise += $exerciseTime;
-        }
+    // Store meals data for this day
+    $mealsData[] = $mealNumbers;
+}
 
-        $targetExercise = 100;
-        $exercisePercentage = 100 * $exerciseTime / $targetExercise;
+// Exercise data
+$stmt = "SELECT * FROM patientsexerciselog WHERE userID='$userID'  ";
+$sql3 = mysqli_query($conn, $stmt);
 
-        // Store exercise data for this day
-        $exerciseData[] = $exerciseTime;
+if (mysqli_num_rows($sql3) > 0) {
+    while ($row = mysqli_fetch_array($sql3)) {
+        $exerciseTime = $row['exerciseDuration'];
+        $totalExercise += $exerciseTime;
     }
 
+    $targetExercise = 100;
+    $exercisePercentage = 100 * $exerciseTime / $targetExercise;
+
+    // Store exercise data for this day
+    $exerciseData[] = $exerciseTime;
+}
+
+// Check if there is no data in any of the tables and return null for predictedRiskLevel
+if (empty($sleepData) && empty($mealsData) && empty($exerciseData)) {
+    $predictedRiskLevel = null;
+} else {
     // Calculate and store risk levels (0, 1, or 2) based on your criteria
     $riskLevel = calculateRiskLevel($sleepPercentage, $mealsPercentage, $exercisePercentage);
     $riskLevels[] = $riskLevel;
 
+    // Now, use the KNearestNeighbors classifier to predict the risk level
+    $samples = [[$sleepPercentage, $mealsPercentage, $exercisePercentage]];
+    $labels = [0]; // Use a placeholder label, as it's not relevant for prediction
 
-// Convert PHP arrays to JavaScript arrays
-$sleepDataJSON = json_encode($sleepData);
-$mealsDataJSON = json_encode($mealsData);
-$exerciseDataJSON = json_encode($exerciseData);
-$riskLevelsJSON = json_encode($riskLevels);
+    $classifier = new KNearestNeighbors();
+    $classifier->train($samples, $labels);
+
+    // Use the classifier to predict the risk level for the latest data
+    $latestData = [$sleepPercentage, $mealsPercentage, $exercisePercentage];
+    $predictedRiskLevel = $classifier->predict($latestData);
+}
 
 // Define a function to calculate the risk level based on criteria
-$riskLevel = 0;
 function calculateRiskLevel($sleepPercentage, $mealsPercentage, $exercisePercentage) {
     // Define your criteria and calculations here to determine the risk level (0, 1, or 2)
     // Example criteria:
     if ($sleepPercentage >= 80 && $mealsPercentage >= 80 && $exercisePercentage >= 80) {
-        $riskLevel = 0; // Low risk
+        return 0; // Low risk
     } elseif ($sleepPercentage >= 60 && $mealsPercentage >= 60 && $exercisePercentage >= 60) {
-        $riskLevel = 1; // Moderate risk
+        return 1; // Moderate risk
     } else {
-        $riskLevel = 2; // High risk
+        return 2; // High risk
     }
 }
+
+// You can now use $predictedRiskLevel as the predicted risk level for the latest data or null if there is no data.
 ?>
